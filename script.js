ac2adfedc54a37d7332738b0e8710b75606ae64a
Max Simpson <maxsimpson95@gmail.com>
2020-03-20 20:19:56 +0800

    ajax modified, icon get, card create

--- script.js
+++ script.js
@@ -34,29 +34,85 @@ let storeRecentSearch = function (cityWeatherObject){
 let mapAPIWeatherToLocalWeather = function (cityWeatherDTO) {
     //this is seperated so that if the API change the format of their object we should just edit here
 
-    var queryURL = 
-    "api.openweathermap.org/data/2.5/forecast?q=" + 
-    cityName + 
-    "appid=" + APIKEY
+    
+}
 
+let searchCityWeatherByAPI = async function(cityName){
+    //construct a request and search the api. then return the api weather object
+    var queryURL = 
+    "https://api.openweathermap.org/data/2.5/forecast?q=" + 
+    cityName + 
+    "&units=metric" +
+    "&appid=" + APIKEY
+
+    console.log({queryURL});
+    // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+    //the only issue is the daily forecast needs a paid subscription, so we get a 3 hourly
+
+    var cityWeather = null;
     $.ajax({
     url: queryURL,
     method: "GET"
     })
-    .then(function(response) {
-        console.log({response})
-        return response;
-    });
+    .then(renderWeather,null);
+    //then just wants the name of the call back functions for args, not an actual call itself - no ()
+}
+
+let renderWeather = function(weatherAjaxResponse){
     
+    //get the weather for 12 noon for the day, instead of every 3 hours
+    weatherArray = weatherAjaxResponse.list.filter(function (item) {
+        return item.dt_txt.endsWith("12:00:00");
+    });
+
+    //empty any current cards
+    $("#forecast-cards").empty();
+
+    //create a local weather object from the weatherDTO
+    weatherArray.forEach(element => {
+        console.log({element});
+        forecast = {
+            "temp": element.main.temp,
+            "humidity": element.main.humidity,
+            "weatherSummary": element.weather[0].main,
+            "weatherDescription": element.weather[0].description,
+            "iconSrc": "http://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png"
+        };
+
+        //clear the cards from underneath forecast cards
+        
+        addWeatherCard(forecast)
+    });
+
+    var cardUpdatedText = $("<small>").text("Last updated: " + (new Date().toLocaleString())).addClass("text-muted p-3");
+    $("#forecast-cards").append(cardUpdatedText);
+
+
 }
 
-let searchCityWeatherByAPI = function(){
-    //construct a request and search the api. then return the api weather object
+let addWeatherCard = function(forecast){
+    //create a div that uses bootstraps card class using jquery
+    var card = $("<div>").addClass("col-2");
+    var cardImage = $("<img>").attr("src",forecast.iconSrc)//.addClass("card-img-top")
+    var cardBody = $("<div>");
+    var cardTitle = $("<h5>");
+    var cardTemp = $("<p>").text("Temp: " + forecast.temp);
+    var cardHumidity = $("<p>").text("Humidity: " + forecast.humidity + "%");
+    
 
-    return cityWeatherDTO;
+    cardBody.append(cardTitle).append(cardTemp).append(cardHumidity)//.append(cardUpdatedText);
+    card.append(cardImage).append(cardBody);
+    $("#forecast-cards").append(card);
 }
 
-let renderWeather = function(){
-
+let search = function(event){
+    console.log("search function")
+    console.log( $(this).parent);
+    $()
+    // $(this)
 }
 
+$("#search-button").on("click",search);
+
+// var a = searchCityWeatherByAPI("Perth");
+// console.log({a});
