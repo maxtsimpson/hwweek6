4ee9fd2591abea1e2b3b7d9f02ca543e805b05ff
Max Simpson <maxsimpson95@gmail.com>
2020-03-22 00:19:00 +0800

    added additional API. changed bootswatch theme

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // had a look at trying to do this as a class instead but doesn't really warrant it because
 //there's no real calculations associated with it and want the render functions seperate
 let cityWeatherRepo = {};
+let currentCity = "";
 const APIKEY = "db7659b069972354627d855668ebaf95"
-
+const GEOAPIFYKEY = "d167547b4c204380b2554416dc375bc8"
 
 let retrieveRecentSearches = function () {
     //get the cityWeatherRepo from local storage. if there isnt one then create a blank object
@@ -25,9 +26,8 @@ let storeRecentSearch = function (cityWeatherObject){
     
     //we are only going to store one weather report for one city at a time
     //no historical data or duplicates
-    cityWeatherRepo[cityWeatherObject.Name] = {
-
-    }
+    console.log({cityWeatherObject});
+    cityWeatherRepo[cityWeatherObject.Name.toLowerCase()] = cityWeatherObject;
     localStorage.setItem("cityWeatherRepo",JSON.stringify(cityWeatherRepo));
 }
 
@@ -38,6 +38,8 @@ let mapAPIWeatherToLocalWeather = function (cityWeatherDTO) {
 }
 
 let searchCityWeatherByAPI = async function(cityName){
+    console.log({cityName});
+    
     //construct a request and search the api. then return the api weather object
     var queryURL = 
     "https://api.openweathermap.org/data/2.5/forecast?q=" + 
@@ -49,29 +51,59 @@ let searchCityWeatherByAPI = async function(cityName){
     // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
     //the only issue is the daily forecast needs a paid subscription, so we get a 3 hourly
 
-    var cityWeather = null;
     $.ajax({
     url: queryURL,
     method: "GET"
     })
-    .then(renderWeather,null);
+    .then(renderWeatherFromAjax,null);
     //then just wants the name of the call back functions for args, not an actual call itself - no ()
 }
 
-let renderWeather = function(weatherAjaxResponse){
+let getCityLongtitudeAndLatitudeByAPI = async function(cityName){
+    var queryURL = "https://geoapify-platform.p.rapidapi.com/v1/geocode/search" +
+    "?text=" + cityName +
+    "&type=city&apiKey=" + GEOAPIFYKEY
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+        })
+        .then(getCityUVIndex,null);
+
+}
+
+let getCityUVIndex = function(longtitude,latitude) {
+    
+    //date in the format 2017-01-02T12:00:00Z
+    var currentDate = 'P';
+    
+    var queryURL = 
+    "https://api.openweathermap.org/v3/uvi/" + 
+    latitude + "," + longtitude + 
+    currentDate + 
+    ".json?" + 
+    "appid=" + APIKEY
+
+    $.ajax({
+    url: queryURL,
+    method: "GET"
+    })
+    .then(renderUVFromAjax,null);
+}
+
+let renderWeatherFromAjax = function(weatherAjaxResponse){
     
     //get the weather for 12 noon for the day, instead of every 3 hours
     weatherArray = weatherAjaxResponse.list.filter(function (item) {
         return item.dt_txt.endsWith("12:00:00");
     });
 
-    //empty any current cards
-    $("#forecast-cards").empty();
-
+    var forecastArray = [];
     //create a local weather object from the weatherDTO
     weatherArray.forEach(element => {
         console.log({element});
         forecast = {
+            "date": element,
             "temp": element.main.temp,
             "humidity": element.main.humidity,
             "weatherSummary": element.weather[0].main,
@@ -79,25 +111,57 @@ let renderWeather = function(weatherAjaxResponse){
             "iconSrc": "http://openweathermap.org/img/wn/" + element.weather[0].icon + "@2x.png"
         };
 
-        //clear the cards from underneath forecast cards
-        
-        addWeatherCard(forecast)
+        forecastArray.push(forecast);     
     });
 
+    console.log({forecastArray});
+    renderForecastArray(forecastArray);
+
     var cardUpdatedText = $("<small>").text("Last updated: " + (new Date().toLocaleString())).addClass("text-muted p-3");
-    $("#forecast-cards").append(cardUpdatedText);
+    $("#update-time").append(cardUpdatedText);
 
+    var cityWeatherObject = {
+        "Name": currentCity,
+        "ForecastArray": forecastArray
+    };
 
+    storeRecentSearch(cityWeatherObject);
+}
+
+let renderRecentSearch = function(cityName){
+    // console.log($("#search-history-list").each(function(){
+    //     if ($(this).text().toLowerCase() === cityName.toLowerCase()) {
+    //         $("#search-history-list").remove()       
+    //     }
+    // }));
+
+    $("#search-history-list").remove(":contains(cityName)")
+    
+    $("#search-history-list").prepend(
+        $("<li>").append($("<a>").text(cityName).attr("href","#").attr("id",cityName))
+        .addClass("list-group-item")
+    );
+}
+
+let renderForecastArray = function(forecastArray) {
+    console.log({forecastArray});
+    //empty any current cards
+    $("#forecast-cards").empty();
+    forecastArray.forEach(forecast => {
+        addWeatherCard(forecast)    
+    });
+    $("#update-time").empty()
 }
 
 let addWeatherCard = function(forecast){
     //create a div that uses bootstraps card class using jquery
-    var card = $("<div>").addClass("col-2");
-    var cardImage = $("<img>").attr("src",forecast.iconSrc)//.addClass("card-img-top")
-    var cardBody = $("<div>");
-    var cardTitle = $("<h5>");
-    var cardTemp = $("<p>").text("Temp: " + forecast.temp);
-    var cardHumidity = $("<p>").text("Humidity: " + forecast.humidity + "%");
+    var card = $("<div>").addClass("card border-primary bg-info mb-3");
+    var cardImage = $("<img>").attr("src",forecast.iconSrc).addClass("card-img-top img-fluid")
+    var cardHeader = $("<div>").addClass("card-header");
+    var cardBody = $("<div>").addClass("card-body");
+    var cardTitle = $("<h5>").addClass("card-title");
+    var cardTemp = $("<p>").text("Temp: " + forecast.temp).addClass("card-text text-light");
+    var cardHumidity = $("<p>").text("Humidity: " + forecast.humidity + "%").addClass("card-text text-light");
     
 
     cardBody.append(cardTitle).append(cardTemp).append(cardHumidity)//.append(cardUpdatedText);
@@ -105,14 +169,33 @@ let addWeatherCard = function(forecast){
     $("#forecast-cards").append(card);
 }
 
-let search = function(event){
-    console.log("search function")
-    console.log( $(this).parent);
-    $()
-    // $(this)
+let renderUVFromAjax = function(uvIndex){
+    $(".jumbotron").remove(":contains('UV')")
+    var jumboUVIndex = $("<p>").text("UV Index: " + uvIndex)
+    $(".jumbotron").append(jumboUVIndex);
 }
 
-$("#search-button").on("click",search);
+let populateWeatherCard = function (forecast) {
+    
+    var jumboTemp = $("<p>").text("Temperature: " + forecast.temp + '\u00B0' + "C").addClass("");
+    var jumboHumidity = $("<p>").text("Humidity: " + forecast.humidity + "%").addClass("");
+    var jumboWindSpeed = $("<p>").text("Humidity: " + forecast.humidity + "%").addClass("");
+    
+    $(".jumbotron").append(jumboTemp).append(jumboHumidity).append(jumboWindSpeed)
+}
 
-// var a = searchCityWeatherByAPI("Perth");
+$("#search-button").on("click",function(){
+    currentCity = $("#search-term").val();
+    renderRecentSearch(currentCity);
+    searchCityWeatherByAPI(currentCity);
+});
+
+$("#search-history-list").on("click",function(){
+    // console.log();
+    currentCity = $(event.target).text();
+    renderRecentSearch(currentCity);
+    searchCityWeatherByAPI(currentCity);
+});
+
+// var a = searchCityWeatherByAPI("perth");
 // console.log({a});
